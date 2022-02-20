e6bfc80ca209f92615aca69e04c17358d99665f4
Alper <alper.sonalp@gmail.com>
2022-02-20 15:48:07 +0300

    ödevler

---
+++ Odev1702-1/Program.cs
@@ -0,0 +1,10 @@
+﻿int a = 3;
+int b = 2;
+
+
+b = b + a;
+a = b - a;
+b = b - a;
+
+Console.WriteLine($"a'nın değeri {a}"); // 2
+Console.WriteLine($"b'nin değeri {b}"); // 3
---
+++ Odev1702-2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Kullanıcıdan 5 adet benzersiz sayı alalım..
+// bu sayıların;
+// en büyüğünü
+// en küçüküğünü
+// ortalamasını ekrana yazdıralım..
+
+// Ödev: bu örneğin aynısını for döngüsü ile yapınız.. Not : for'un yapısını bozmadan...
+//for(int i=0; i<sayis.Length;i++)
+
+
+int n = 5;
+int[] sayis = new int[n];
+
+int i = 0;
+for (i=0; i< sayis.Length; i++)
+{
+
+  
+    Console.WriteLine($"Lütfen ({i + 1}). sayı giriniz");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+
+    if (Array.IndexOf(sayis, sayi) == -1) // sayi'yi sayisde ara..
+    {
+        sayis[i] = sayi;
+    }
+}
+Console.WriteLine($"en büyüğü {sayis.Max()}");
+Console.WriteLine($"en küçüüğü {sayis.Min()}");
+Console.WriteLine($"ortalaması {sayis.Average()}");
