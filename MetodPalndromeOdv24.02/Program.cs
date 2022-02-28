35c231484868557aeabc14b691e88dc5d86ca6ed
Alper <alper.sonalp@gmail.com>
2022-02-28 12:30:46 +0300

    ödev

---
+++ MetodPalndromeOdv24.02/Program.cs
@@ -0,0 +1,31 @@
+﻿class Program
+{
+
+    static void Main(string[] args)
+    {
+        string cumle = "bu gün hava çok güzel arkadaşlar ile pinkik yapmaya gittik orada kavak ağaçları vardı çıkmadan önce elbiselerime ütü yaptım";
+
+        string[] palindroms = cumle.Split(' ');
+
+        foreach (var item in palindroms)
+        {
+
+            if (IsPalindrome(item))
+                Console.WriteLine(item);
+        }
+        static string Palindrome(string kelime)
+        {
+            char[] temp = kelime.ToCharArray();
+            Array.Reverse(temp);
+
+            return new string(temp);
+        }
+
+        static bool IsPalindrome(string kelime)
+        {
+            bool durum = Palindrome(kelime) == kelime ? true : false;
+            return durum;
+        }
+    }
+}
+
